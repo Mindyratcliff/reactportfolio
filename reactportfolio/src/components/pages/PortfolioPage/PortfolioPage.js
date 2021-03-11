f1c82a6690b33d8f468a54a531c5d1cca45850dd
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2021-03-11 16:12:08 -0500

    Added text for Project 2 to portfolio page

--- reactportfolio/src/components/pages/PortfolioPage/PortfolioPage.js
+++ reactportfolio/src/components/pages/PortfolioPage/PortfolioPage.js
@@ -4,7 +4,23 @@ import "./portfoliopage.css";
 const PortfolioPage = () => (
   <div>
     <h1>Portfolio</h1>
-   
+    <div class="row">
+      <h3>Happy Trees</h3>
+      <img src="" alt=""/>
+      <p>
+        A drawing application which allows users to save drawings in a personal
+        account using Passport JS. This uses PaperJS along with Sequelize to
+        connect to a MySQL database. This was a group project completed with
+        Abdinajib Liban, Kole Gasior, Simon Ziccardi, and Efraín González. I
+        worked on the MySQL database, Sequelize, the front-end, logo, and wrote
+        the ReadMe file. Happy Trees can be accessed here{" "}
+        <a href="https://guarded-ravine-40907.herokuapp.com">Happy Trees</a>
+        The GitHub Repository for Happy Trees can be accessed here{" "}
+        <a href="https://github.com/Mindyratcliff/Project2">
+          Happy Trees Repo.
+        </a>
+      </p>
+    </div>
   </div>
 );
 
