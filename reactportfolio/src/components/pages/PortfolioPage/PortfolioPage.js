61b019e6fc3e6d9da5d38b29311fa0c3bf533036
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2021-03-11 16:19:17 -0500

    Added text for portfolio work

--- reactportfolio/src/components/pages/PortfolioPage/PortfolioPage.js
+++ reactportfolio/src/components/pages/PortfolioPage/PortfolioPage.js
@@ -6,7 +6,7 @@ const PortfolioPage = () => (
     <h1>Portfolio</h1>
     <div class="row">
       <h3>Happy Trees</h3>
-      <img src="" alt=""/>
+      <img src="" alt="" />
       <p>
         A drawing application which allows users to save drawings in a personal
         account using Passport JS. This uses PaperJS along with Sequelize to
@@ -18,6 +18,41 @@ const PortfolioPage = () => (
         The GitHub Repository for Happy Trees can be accessed here{" "}
         <a href="https://github.com/Mindyratcliff/Project2">
           Happy Trees Repo.
+        </a>
+      </p>
+    </div>
+    <div class="row">
+      <h3>CoronaDate</h3>
+      <img src="" alt="" />
+      <p>
+        This is a web application which allows users to search both The Covid
+        Tracking Project and the Ticketmaster APIs so they can make an informed
+        decision about attending events in their state. This was a group project
+        completed with the assistance of Mengmei Tu, Juan Munoz, and Evan
+        Robinson. I worked on the API call to Ticketmaster, made the hero image
+        and logo, and wrote the ReadMe file. CoronaDate can be accessed here{" "}
+        <a href="https://mmeii.github.io/project-1-corona-date/">CoronaDate</a>
+        The GitHub Repository for CoronaDate can be accessed here{" "}
+        <a href="https://github.com/mmeii/project-1-corona-date">
+          CoronaDate Repo.
+        </a>
+      </p>
+    </div>
+    <div class="row">
+      <h3>Budget Tracker - Progressive Web Application</h3>
+      <img src="" alt="" />
+      <p>
+        This is a Progressive Web Application which allows users to track
+        expenses and deposits online and offline. Webpack and Webpack PWA
+        manifest are used to create the manifest, and IndexedDB is used for the
+        offline transactions when the MongoDB/Mongoose database is not
+        available. This Budget Tracker can be accessed here{" "}
+        <a href="https://ratcliffbudgettracker.herokuapp.com">
+          Budget Tracker.
+        </a>
+        The GitHub Repository for this PWA can be accessed here{" "}
+        <a href="https://github.com/Mindyratcliff/budgettracker">
+          Budget Tracker Repo.
         </a>
       </p>
     </div>
