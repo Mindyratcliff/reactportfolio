bd9f1c06c29d8c56a3d080a0415f92a523cdc1a4
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2021-03-11 14:00:30 -0500

    Added portfolio container shell

---
+++ reactportfolio/src/components/PortfolioContainer.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import Nav from "./Nav";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+class Portfolio extends Component {
+  state = {
+    currentPage: "Home"
+  };
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
+  renderPage = () => {
+    if (this.state.currentPage === "Home") {
+      return <Home />;
+    } else if (this.state.currentPage === "About") {
+      return <About />;
+    } else {
+      return <Contact />;
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <Nav
+          currentPage={this.state.currentPage}
+          handlePageChange={this.handlePageChange}
+        />
+        {this.renderPage()}
+      </div>
+    );
+  }
+}
+
+export default Portfolio;
