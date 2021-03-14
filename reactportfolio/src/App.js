0cd85806aed3a414f4782193e26c9de79b48add1
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2021-03-14 13:16:45 -0400

    Importing react router and including routes to pages in App

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,6 +1,36 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 import PortfolioContainer from "./components/PortfolioContainer";
+import Contact from "./components/pages/Contact/Contact";
+import PortfolioPage from "./components/pages/PortfolioPage/PortfolioPage";
 
-const App = () => <PortfolioContainer />;
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <PortfolioContainer />
+        <Switch>
+          <Route exact path={["/", "/home"]}>
+            <Home />
+          </Route>
+          <Route exact path={["/About", "/About"]}>
+            <About />
+          </Route>
+          <Route exact path={["/Contact", "/Contact"]}>
+            <Contact />
+          </Route>
+          <Route exact path={["/PortfolioPage", "/PortfolioPage"]}>
+            <PortfolioPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
 
 export default App;
