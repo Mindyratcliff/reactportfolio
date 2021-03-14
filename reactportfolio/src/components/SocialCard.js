8034c8e8800be997085a89d85f76867f6c400f75
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2021-03-14 13:26:36 -0400

    Added card component for social media links

---
+++ reactportfolio/src/components/SocialCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function SocialCard() {
+  return (
+    <div class="col-3">
+      <div class="card" style="width: 18rem">
+        <div class="card-body">
+          <h5 class="card-title">Mindy Ratcliff</h5>
+          <h6 class="card-subtitle">Find me on Social Media</h6>
+          <p class="card-text">
+            <ul>
+              <li>
+                <a href="https://www.facebook.com/mindyratcliff/">
+                  <i class="fab fa-facebook"></i> Facebook
+                </a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/realmindyratcliff/">
+                  <i class="fab fa-instagram"></i> Instagram
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/mindy-ratcliff">
+                  <i class="fab fa-linkedin"></i> LinkedIn
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/Mindyratcliff">
+                  <i class="fab fa-github"></i> GitHub
+                </a>
+              </li>
+            </ul>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SocialCard;
--- reactportfolio/src/components/pages/Contact/Contact.js
+++ reactportfolio/src/components/pages/Contact/Contact.js
@@ -1,9 +1,11 @@
 import React from "react";
+import SocialCard from "./SocialCard"
 import "./contact.css";
 
 const Contact = () => (
   <div>
     <h1>Contact Me</h1>
+    <SocialCard/>
    
   </div>
 );
