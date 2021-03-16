aaf157737293ecfb2e8e3e478dffdd8ba1b62ec7
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2021-03-16 14:18:17 -0400

    fixing links for images

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -7,7 +7,7 @@ import Nav from "../../Nav"
 const Home = () => {
   return(
   <div id="landing">
-   <img src="{`${process.env.PUBLIC_URL}/../../../assets/Mindy Ratcliff-3.png`}" id="hero" alt="sun with the words Hi I'm Mindy"/>
+   <img src="{`${process.env.PUBLIC_URL}/assets/Mindy Ratcliff-3.png`}" id="hero" alt="sun with the words Hi I'm Mindy"/>
    <h2>full-stack web developer</h2>
   </div>
   )};
